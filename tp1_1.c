a4640a2aab7aa97ef40ffa8666511fcd6bf5ec23
mendoza91guada <mendoza91guada@gmail.com>
2026-04-02 16:30:38 -0300

    Ahora sí es el ejercicio 4 (el commit anterior era del 3)

--- tp1_1.c
+++ tp1_1.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
-void main{
+void main(){
+    int x, *p;
+    x=125;
+    p=&x;
+
     printf("Hola mundo\n");
+    printf("\n-----------------\n");
+
+    printf("\nEl contenido del puntero es: %p", p);
+    printf("\nLa dirección almacenada por el puntero es: %p", p);
+    printf("\nLa dirección de memoria de la variable es: %p", &x);
+    printf("\nLa dirección de memoria del puntero es: %p", &p);
+    printf("\nEl tamaño de la memoria utilizada por la variable es %d", sizeof(x));
+
 }
